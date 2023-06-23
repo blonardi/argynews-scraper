90cffcea83c0eb29ffc24a1c42bc9d2ecbf28579
blonardi <lonardibautista@gmail.com>
2023-06-23 17:33:28 -0300

    Cambios en  parametros de fn para utilizar 2 obj

--- index.js
+++ index.js
@@ -30,14 +30,25 @@ const cors = require("cors");
 //         clase2: "a.headline-link",
 //         clase3: "aria-label",
 //         href: "href",
+//         noticias : [noticia1, noticia2]
 //     },
 // ];
 
-const dataUrl = {
+const dataInfobae = {
+    name: "Infobae",
     url: "https://www.infobae.com",
-    clase1: ".d23-story-card-info",
-    clase2: "a.headline-link",
-    clase3: "aria-label",
+    // clase1: ".d23-story-card-info",
+    clase1: "a.headline-link",
+    clase3: ".d23-story-card-hl",
+    href: "href",
+};
+
+const dataGuardian = {
+    name: "Guardian",
+    url: "https://www.theguardian.com/uk",
+    clase1: ".fc-item__title",
+    clase2: ".fc-item__link",
+    clase3: ".js-headline-text",
     href: "href",
 };
 
@@ -49,35 +60,66 @@ app.get("/", function (req, res) {
     res.json("This is my webscraper with node");
 });
 
-async function solicitudURL(siteData, res) {
+// async function solicitudURL(siteData, res) {
+//     // siteData.forEach((siteObject) => {
+//     const { url, clase1, clase2, clase3, href } = siteData;
+//     try {
+//         await axios(url).then((response) => {
+//             const html = response.data;
+//             const $ = cheerio.load(html);
+//             const articles = [];
+//             $(clase1, html).each(function () {
+//                 // const titulo = $(this).text();
+//                 // const uerrele = $(this).find("a").attr(href);
+//                 const headlinesElement = $(this).find(clase2);
+//                 headlinesElement.each((index, value) => {
+//                     const title = $(value).attr(clase3);
+//                     const urlFirst = $(value).attr(href);
+//                     const urlTotal = `${url}${urlFirst}`;
+//                     // esta es la url completa, en el back anda ok, pero no renderiza en front => undefined
+//                     articles.push({
+//                         title,
+//                         urlTotal,
+//                         // titulo,
+//                         // uerrele,
+//                     });
+//                 });
+//             });
+//             const firstArticles = articles.slice(0, 5);
+//             res.json(firstArticles);
+//         });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({
+//             error: "Ocurrió un error en la solicitud.",
+//         });
+//     }
+// }
+
+async function solicitudUrlNytimes(siteData, res) {
     // siteData.forEach((siteObject) => {
-    const { url, clase1, clase2, clase3, href } = siteData;
+    const { name, url, clase1, clase2, clase3, href } = siteData;
     try {
         await axios(url).then((response) => {
             const html = response.data;
             const $ = cheerio.load(html);
             const articles = [];
             $(clase1, html).each(function () {
-                // const titulo = $(this).text();
-                // const uerrele = $(this).find("a").attr(href);
-                //esto es codigo de mi amigo chatgpt retocado por mi.
-                const headlinesElement = $(this).find(clase2);
-                headlinesElement.each((index, value) => {
-                    const title = $(value).attr(clase3);
-                    const urlFirst = $(value).attr(href);
-                    const urlTotal = `${url}${urlFirst}`;
-                    // esta es la url completa, en el back anda ok, pero no renderiza en front => undefined
-                    articles.push({
-                        title,
-                        urlTotal,
-                        // titulo,
-                        // uerrele,
-                    });
+                const item = $(this).find(clase2);
+                const url = item.attr(href) || $(this).attr(href);
+                const title =
+                    item.find(clase3).text() || $(this).find(clase3).text();
+
+                // esta es la url completa, en el back anda ok, pero no renderiza en front => undefined
+                articles.push({
+                    title,
+                    url,
                 });
             });
             const firstArticles = articles.slice(0, 5);
             // console.log(firstArticles);
             res.json(firstArticles);
+            return { site: name, articles };
         });
     } catch (err) {
         console.error(err);
@@ -85,11 +127,11 @@ async function solicitudURL(siteData, res) {
             error: "Ocurrió un error en la solicitud.",
         });
     }
-    // });
 }
 
 app.get("/results", (req, res) => {
-    solicitudURL(dataUrl, res);
+    // solicitudUrlNytimes(dataInfobae, res);
+    solicitudUrlNytimes(dataGuardian, res);
 });
 
 app.listen(PORT, () =>
