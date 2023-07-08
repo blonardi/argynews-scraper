27fde819ce404c35fc143e67d36faabb1cdc1b85
blonardi <lonardibautista@gmail.com>
2023-07-07 23:48:05 -0300

    Cambios de prueba en vercel

--- server/src/app.js
+++ server/src/app.js
@@ -1,37 +1,36 @@
 // const { data } = require("cheerio/lib/api/attributes");
 
-const feedDisplay = document.querySelector("#feed");
+// const feedDisplay = document.querySelector("#feed");
 
-const myUrl = "http://localhost:8000/results";
+// const myUrl = "http://localhost:8000/results";
 
-/* fetch("http://localhost:8000/results")
-    .then((response) => response.json())
-    .then((data) => { allArticles.push(data)
-        
-        data.forEach((article) => {
-            console.log(article.url);
-            const articleItem =
-                `<div style="width:100%"><h3>` +
-                article.titulo +
-                `</h3><p>` +
-                article.uerrele +
-                `</p></div>`;
-            feedDisplay.insertAdjacentHTML("beforeend", articleItem);
-        });
-    })
-    .catch((err) => console.log(err)); */
+// /* fetch("http://localhost:8000/results")
+//     .then((response) => response.json())
+//     .then((data) => { allArticles.push(data)
 
-async function getArticles () {
-    const response = await fetch(myUrl)
-    const allArticles = await response.json()
-    return allArticles
-}
+//         data.forEach((article) => {
+//             console.log(article.url);
+//             const articleItem =
+//                 `<div style="width:100%"><h3>` +
+//                 article.titulo +
+//                 `</h3><p>` +
+//                 article.uerrele +
+//                 `</p></div>`;
+//             feedDisplay.insertAdjacentHTML("beforeend", articleItem);
+//         });
+//     })
+//     .catch((err) => console.log(err)); */
 
+// async function getArticles () {
+//     const response = await fetch(myUrl)
+//     const allArticles = await response.json()
+//     return allArticles
+// }
 
-async function createArticles () {
-    const allArticles = await getArticles()
-    const infobae = allArticles.filter(item => item[0] === 'Infobae')
-    const guardian = allArticles.filter(item => item[0] === 'Guardian')
-}
+// async function createArticles () {
+//     const allArticles = await getArticles()
+//     const infobae = allArticles.filter(item => item[0] === 'Infobae')
+//     const guardian = allArticles.filter(item => item[0] === 'Guardian')
+// }
 
-createArticles()
+// createArticles()
