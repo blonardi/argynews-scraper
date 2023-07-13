1e2683f6c67b03efc80368d8672bdcf69879b716
antoninosartori <sartoriantonino@gmail.com>
2023-07-13 12:31:54 -0300

    no hice nada

--- server/indexPup.js
+++ server/indexPup.js
@@ -1,107 +1,53 @@
 import puppeteer, { PuppeteerNode } from "puppeteer";
+async function scrollDownAndGetImages(urlSite, titleClass) {
+   // const browser = await puppeteer.launch();
+   const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 200,
+   });
+   const page = await browser.newPage();
 
-// async function openWebPage() {
-//     const browser = await puppeteer.launch({
-//         headless: false,
-//         slowMo: 200,
-//     });
-//     const page = await browser.newPage();
-//     await page.goto("https://google.com");
-//     await browser.close();
-// }
+   // await page.goto("https://ambito.com");
+   await page.goto(urlSite);
 
-// openWebPage();
+   // Función que se ejecutará en el contexto de la página
+   const scrollDown = async () => {
+      // Desplázate hacia abajo hasta el final de la página
+      await new Promise((resolve) => {
+         const distance = 100; // Distancia de desplazamiento
+         const delay = 100; // Retardo entre desplazamientos
+         const timer = setInterval(() => {
+            window.scrollBy(0, distance);
+            if (
+               window.innerHeight + window.pageYOffset >=
+               document.body.scrollHeight
+            ) {
+               clearInterval(timer);
+               resolve();
+            }
+         }, delay);
+      });
+   };
 
-// async function getData() {
-//     const browser = await puppeteer.launch({
-//         headless: false,
-//         slowMo: 200,
-//     });
-//     const page = await browser.newPage();
-//     // await page.goto("https://ambito.com");
-//     await page.goto("https://ambito.com", { waitUntil: "networkidle0" });
+   // Ejecuta la función de desplazamiento en la página
+   await page.evaluate(scrollDown);
 
-//     // await page.waitForSelector(".news-article figure .figure-img");
+   const cards = await page.$$(".d23-story-card-ctn");
 
-//     const result = await page.evaluate(() => {
-//         let imgs = document.querySelectorAll(
-//             ".news-article > figure > a > .figure-img"
-//         );
-//         const array = [...imgs];
-//         // console.log(array);
-//         const imagenesObj = [];
-//         const imagenes = array.map((img, i) => {
-//             let src = img.getAttribute("src");
-//             imagenesObj.push([i, src]);
-//         });
-//         return imagenesObj;
-//     });
+   let container = [];
+   for (let card of cards) {
+      const title = card.querySelector('.headline-link h2')
+      container.push(title)
+   }
 
-//     console.log(result);
-//     await browser.close();
-// }
+   console.log(container)
 
-// getData();
+   /*    const imageUrls = await page.$$eval(
+         imageClass,
+         (imgs) => imgs.map((img) => img.src)
+      ); */
 
-async function scrollDownAndGetImages() {
-    // const browser = await puppeteer.launch();
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 200,
-    });
-    const page = await browser.newPage();
-
-    await page.goto("https://ambito.com");
-
-    // Función que se ejecutará en el contexto de la página
-    const scrollDown = async () => {
-        // Desplázate hacia abajo hasta el final de la página
-        await new Promise((resolve) => {
-            const distance = 100; // Distancia de desplazamiento
-            const delay = 100; // Retardo entre desplazamientos
-            const timer = setInterval(() => {
-                window.scrollBy(0, distance);
-                if (
-                    window.innerHeight + window.pageYOffset >=
-                    document.body.scrollHeight
-                ) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, delay);
-        });
-    };
-
-    // Ejecuta la función de desplazamiento en la página
-    await page.evaluate(scrollDown);
-
-    // Extrae las imágenes después de realizar el desplazamiento
-    // const imagenesObj = [];
-    // const imageUrls = await page.$$eval(
-    //     ".news-article > figure > a > .figure-img",
-    //     (imgs) => imgs.map((img) => imagenesObj.push(img.src))
-    // );
-
-    // const imgsSrc = await page.$$eval(
-    //     ".news-article > figure > a > .figure-img",
-    //     (imgs) =>
-    //         imgs.map((img, i) => {
-    //             let src = img.getAttribute("src");
-    //             imagenesObj.push({i, src});
-    //         })
-    //     return imagenesObj;
-    // );
-    // imageUrls();
-    // console.log(imagenesObj);
-
-    const imageUrls = await page.$$eval(
-        ".news-article > figure > a > .figure-img",
-        (imgs) => imgs.map((img) => img.src)
-    );
-
-    console.log(imageUrls);
-
-    await browser.close();
+   await browser.close();
 }
-
-scrollDownAndGetImages();
+// scrollDownAndGetImages("https://ambito.com",".news-article > figure > a > .figure-img");
+scrollDownAndGetImages("https://infobae.com", ".headline-link h2");
