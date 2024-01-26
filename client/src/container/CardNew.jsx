4e558da1824a8e9b80cc7949676463103f907201
blonardi <lonardibautista@gmail.com>
2024-01-26 12:31:43 -0300

    Fix typos and extra conditional undefined

--- client/src/container/CardNew.jsx
+++ client/src/container/CardNew.jsx
@@ -9,14 +9,17 @@ export default function CardNew({  urlSite, firstArticles }) {
         {
           firstArticles.map(article => {
             const { title, image, url } = article
-            const fixedUrl = url === 'undefined' ? urlSite : url
+						//console.log({url})
+            //const fixedUrl = url === 'undefined' ? urlSite : url
+						const fixedUrl = (url !== undefined && url.startsWith('/')) ? `${urlSite}${url}` : url;
+						console.log({fixedUrl})
             const href = fixedUrl.startsWith('/') ? `${urlSite}${url}` : url
-
+						
             return(
               <article className='card-new' key={title}>
                 <a href={href} target='_blank' rel='noreferrer' >
-                  < TitleNew title={title} />
-                  < Image imageUrl={image} alt={title} />
+                  <TitleNew title={title} />
+                  <Image imageUrl={image} alt={title} />
                 </a>
               </article>
             )
